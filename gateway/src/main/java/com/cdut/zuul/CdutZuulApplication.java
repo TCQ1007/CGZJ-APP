e8ee20b39640cdb243dabb4c16d17077eb47dd07
TCQ1007 <1007281088@qq.com>
2020-07-28 09:27:03 +0800

    提交项目结构目录

--- gateway/src/main/java/com/cdut/zuul/CdutZuulApplication.java
+++ gateway/src/main/java/com/cdut/zuul/CdutZuulApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableZuulProxy
+@CrossOrigin
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CdutZuulApplication {
